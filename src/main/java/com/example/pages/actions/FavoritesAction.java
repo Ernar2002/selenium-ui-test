c5654d74501a999cde3fd8146ce166d4adc89d5b
ZHAN <zhankuatuly1@gmail.com>
2022-09-30 21:26:20 +0600

    added favorites test

---
+++ src/main/java/com/example/pages/actions/FavoritesAction.java
@@ -0,0 +1,59 @@
+package com.example.pages.actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FavoritesAction {
+    private WebDriver driver;
+
+    private WebDriverWait wait;
+
+    private Actions actions;
+
+    public FavoritesAction(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @FindBy(xpath = "//a[@data-analytics-id=\"roe-mwgbl-winteressentials-mulink\"]")
+    public WebElement showProducts;
+    @FindBy(xpath = "//button[@class=\"mm1gcXw\"]")
+    public List<WebElement> addToFavorites;
+    @FindBy(xpath = "//a[@aria-label=\"Saved Items\"]")
+    public WebElement myFavorites;
+    @FindBy(xpath = "//button[@aria-label=\"Delete\"]")
+    public List<WebElement> deleteMyFavorites;
+
+    public void addProductToFavorites(int index) {
+       //Click to see the products
+        wait.until(ExpectedConditions.visibilityOf(showProducts));
+        showProducts.click();
+        //Add 3 products to favorites
+        wait.until(ExpectedConditions.visibilityOf(addToFavorites.get(1)));
+        WebElement category = addToFavorites.get(index);
+        category.click();
+        wait.until(ExpectedConditions.visibilityOf(myFavorites));
+        myFavorites.click();
+        }
+
+    public void deleteFromFavorites(int index){
+        wait.until(ExpectedConditions.visibilityOf(myFavorites));
+        myFavorites.click();
+        //Delete all favorites
+        wait.until(ExpectedConditions.visibilityOf(deleteMyFavorites.get(0)));
+        for(WebElement delete : deleteMyFavorites){
+           delete.click();
+       }
+    }
+}
---
+++ src/test/java/com/example/pages/actions/FavoritesActionTest.java
@@ -0,0 +1,28 @@
+package com.example.pages.actions;
+
+import com.example.core.BaseTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FavoritesActionTest extends BaseTest {
+
+    public FavoritesAction favoritesAction;
+
+    @BeforeClass
+    public void initialization(){
+        //init classes
+        favoritesAction = new FavoritesAction(driver);
+    }
+
+    @Test
+    public void addProductToFavorites() {
+        favoritesAction.addProductToFavorites(2);
+//        favoritesAction.addProductToFavorites(3);
+//        favoritesAction.addProductToFavorites(4);
+    }
+
+//    @Test
+//    public void deleteFromFavorites() {
+//        favoritesAction.deleteFromFavorites(0);
+//    }
+}
